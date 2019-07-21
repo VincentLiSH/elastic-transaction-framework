417b31c66487ea8f41b5f009d565306724afc091
vincent lee <49069554@qq.com>
2019-07-21 19:32:47 +0800

    remove etf project

--- ETF/src/cn/panshi/etf/core/EtfException4TransQueryNoResult.java
+++
@@ -1,5 +0,0 @@
-package cn.panshi.etf.core;
-
-@SuppressWarnings("serial")
-public class EtfException4TransQueryNoResult extends Exception {
-}
--- ETF/src/cn/panshi/etf/core/EtfManager.java
+++
@@ -1,8 +0,0 @@
-package cn.panshi.etf.core;
-
-/**
- * 这里实现ETF的一些非框架性的逻辑，提供一些外围功能，如：暴露运行时ETF交易类型列表、ETF交易记录查询、暴露ETF的重试和交易查询功能
- */
-public class EtfManager {
-
-}
