9bf90a3484a1f37fd91fe90a65eaf4e2c286e407
49069 <49069@DESKTOP-E8D4PJG>
2019-08-16 17:40:03 +0800

    javadoc

--- ETF4J/src/main/java/cn/panshi/etf4j/redis/IRedisNxLockProtected.java
+++ ETF4J/src/main/java/cn/panshi/etf4j/redis/IRedisNxLockProtected.java
@@ -1,5 +1,9 @@
 package cn.panshi.etf4j.redis;
 
+/**
+ * 用于封装需要redis nx排它锁保护的业务逻辑，用于AbstractRedisLockTemplate.doBizWithinLockProtection回调。
+ * 适用于简单业务场景（无返回值 或单一返回值逻辑）
+ */
 public interface IRedisNxLockProtected {
 	void doBizOnLockSuccess();
 
